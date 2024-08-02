92fae4b66f7c887b85824e217c2bf1c680363a75
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2024-08-02 18:23:12 +0200

    MOD data analysis

--- isdleda/launchers/launch_ledaparams_data_analysis.py
+++ isdleda/launchers/launch_ledaparams_data_analysis.py
@@ -10,6 +10,22 @@ import os
 from isdleda.utils.export.export import load_from_json, save_to_pickle
 from isdleda.utils.paths import OUT_FILES_LEDA_PARAMS, OUT_PLOTS_DATA_DIR
 
+import functools
+import operator
+
+# We want to explore the region around a given lambda; that is, [lamba + val[0], lambda + val[1]]
+C_INTERVALS_FUNCTS = (functools.partial(operator.add, -30),
+                      functools.partial(operator.add, 30))
+# I am less conservative with quantum. If there's a classical speed-up of X,
+# the quantum speed-up would roughly be sqrt(X)
+Q_INTERVALS_FUNCTS = (functools.partial(operator.add, -20),
+                      functools.partial(operator.add, 20))
+# C_INTERVALS_FUNCTS = (functools.partial(operator.mul, .8),
+#                       functools.partial(operator.mul, 1.2))
+# # Bigger confidence interval for quantum since there may be other breakthroughs
+# Q_INTERVALS_FUNCTS = (functools.partial(operator.mul, .7),
+#                       functools.partial(operator.mul, 1.3))
+
 
 def process_ledatools_ledaparams():
     # This one is for LEDA params, the other was for LEDA results
@@ -17,6 +33,8 @@ def process_ledatools_ledaparams():
     q_values = collections.defaultdict(list)
     all_values = collections.defaultdict(list)
     values_by_level = load_from_json(OUT_FILES_LEDA_PARAMS)
+    gacc = 0
+    bound_counts = [0, 0]
     for level, values in values_by_level.items():
         print(f"Level {level}: {len(values)} starting params")
         acc = 0
@@ -29,7 +47,7 @@ def process_ledatools_ledaparams():
                 level_idx = 2
             case _:
                 raise Exception(f"Unknown level {level}")
-        step = 3
+        step = 5
         for i in range(0, len(values), step):
             value = values[i]
             p = value["p"]
@@ -50,14 +68,29 @@ def process_ledatools_ledaparams():
                 "(", "[").replace(")", "]").split(':', 1)[1]
             caes_min, qaes_min = json.loads(complexities_jsonized)['Minimum']
 
-            if .9 * QAES_LAMBDAS[level_idx] < qaes_min < 1.1 * QAES_LAMBDAS[
-                    level_idx] and .9 * AES_LAMBDAS[
-                        level_idx] < caes_min < 1.1 * AES_LAMBDAS[level_idx]:
+            q_bound = False
+            c_bound = False
+            if not(C_INTERVALS_FUNCTS[0](
+                    AES_LAMBDAS[level_idx]) < caes_min < C_INTERVALS_FUNCTS[1](
+                        AES_LAMBDAS[level_idx])):
+                c_bound = True
+                bound_counts[0] += 1
+            if not (Q_INTERVALS_FUNCTS[0](QAES_LAMBDAS[level_idx]
+                                     ) < qaes_min < Q_INTERVALS_FUNCTS[1](
+                                         QAES_LAMBDAS[level_idx])):
+                q_bound = True
+                bound_counts[1] += 1
+            
+            if not q_bound and not c_bound:
                 c_values[rate].append((p, n0, t, v, caes_min))
                 q_values[rate].append((p, n0, t, v, qaes_min))
                 all_values[rate].append((p, n0, t, v))
                 acc += 1
         print(f"For level {level}: {acc} params obtained")
+        gacc += acc
+
+    print(f"Total {gacc} values. (cbound, qbounds): {bound_counts}")
+
     out_file = os.path.join(OUT_PLOTS_DATA_DIR,
                             f"ledatools_ledaparams_exploration_all")
     print(f"Saving to {out_file}")
