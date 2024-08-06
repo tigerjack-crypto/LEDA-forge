10e9295688773347f5abe6f40cbdaf4e7663808c
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2024-08-06 07:44:39 +0200

    FIX restrictions3

--- isdleda/launchers/launch_isd_values_restrictions_3.py
+++ isdleda/launchers/launch_isd_values_restrictions_3.py
@@ -16,6 +16,7 @@ def post_process_values(leda_values_t: Dict[int, Dict[str, List[LEDAValue]]],
     # Assuming all the values are already present in the database
     leda_values_new: Dict[int, List[LEDAValue]] = defaultdict(list)
     isd_values_to_compute: List[ISDValue] = []
+    isd_values_ok: List[ISDValue] = []
     # level idx is (0, 1, 2)
     for level_idx, leda_vals_t_by_p_n0 in leda_values_t.items():
         c_lambda = AES_LAMBDAS[int(level_idx)]
@@ -32,6 +33,7 @@ def post_process_values(leda_values_t: Dict[int, Dict[str, List[LEDAValue]]],
                     assert leda_val.t != -1 and leda_val.v != -1, "Something is wrong"
                     c_complexities = [leda_val.p * leda_val.n0] * 4
                     q_complexities = [leda_val.p * leda_val.n0] * 4
+                    isd_vals_ok = []
 
                     # MRA
                     n = leda_val.p * leda_val.n0
@@ -51,7 +53,7 @@ def post_process_values(leda_values_t: Dict[int, Dict[str, List[LEDAValue]]],
                         continue
                     if cont:
                         continue
-
+                    isd_vals_ok.append(ISDValue(n, k, t))
                     c_complexities[0] = c_aes
                     q_complexities[0] = q_aes
                     del n, k, t
@@ -74,6 +76,9 @@ def post_process_values(leda_values_t: Dict[int, Dict[str, List[LEDAValue]]],
                         continue
                     if cont:
                         continue
+                    c_complexities[1] = c_aes
+                    q_complexities[1] = q_aes
+                    isd_vals_ok.append(ISDValue(n, k, t))
                     del n, k, t
 
                     # KRA 2
@@ -97,6 +102,8 @@ def post_process_values(leda_values_t: Dict[int, Dict[str, List[LEDAValue]]],
                             continue
                         c_complexities[2] = c_aes
                         q_complexities[2] = q_aes
+                        isd_vals_ok.append(ISDValue(n, k, t))
+                        del n, k, t
 
                     # KRA 3
                     n = leda_val.p * leda_val.n0
@@ -111,21 +118,24 @@ def post_process_values(leda_values_t: Dict[int, Dict[str, List[LEDAValue]]],
                         reduction=np.log2(leda_val.p),
                         msg=f"KRA3, {leda_val.p} {leda_val.n0} {leda_val.v}")
                     if (c_aes is None or q_aes is None):
-                        isd_values_to_compute.append(ISDValue(n, n-k, t))
+                        isd_values_to_compute.append(ISDValue(n, n - k, t))
                         continue
                     if cont:
                         continue
-                    
+
                     c_complexities[3] = c_aes
                     q_complexities[3] = q_aes
+                    isd_vals_ok.append(ISDValue(n, k, t))
+
+                    # min c, min q, min c attack, min q attack
                     min_c = n
                     min_q = n
                     min_c_attack = None
                     min_q_attack = None
                     for c_compl, q_compl, c_attack, q_attack in zip(
                             c_complexities, q_complexities,
-                            ("MRA", "KRA1", "KRA2", "KRA3"), ("MRA", "KRA1", "KRA2",
-                                                        "KRA3")):
+                        ("MRA", "KRA1", "KRA2", "KRA3"),
+                        ("MRA", "KRA1", "KRA2", "KRA3")):
                         if c_compl < min_c:
                             min_c = c_compl
                             min_c_attack = c_attack
@@ -133,10 +143,12 @@ def post_process_values(leda_values_t: Dict[int, Dict[str, List[LEDAValue]]],
                             min_q = q_compl
                             min_q_attack = q_attack
                     if is_inside(min_c, min_q, c_lambda, q_lambda):
-                        leda_val.msgs.extend(
-                            [f"MIN C: {min_c} attack: {min_c_attack}, MIN Q:{min_q} attack: {min_q_attack}"])
+                        leda_val.msgs.extend([
+                            f"MIN C: {min_c} attack: {min_c_attack}, MIN Q:{min_q} attack: {min_q_attack}"
+                        ])
                         leda_values_new[int(level_idx)].append(leda_val)
-    return leda_values_new, isd_values_to_compute
+                        isd_values_ok.extend(isd_vals_ok)
+    return leda_values_new, isd_values_ok, isd_values_to_compute,
 
 
 def check_dataset(n, k, t, c_lambda, q_lambda, reduction, msg):
@@ -147,7 +159,7 @@ def check_dataset(n, k, t, c_lambda, q_lambda, reduction, msg):
         data = load_from_json(filename)
         c_time = data['Classic']['Plain']['value'] - reduction
         q_time = (data['Quantum']['Plain']['value']) * 2 - reduction
-        if c_time <= .8 * c_lambda or q_time <= q_lambda:
+        if c_time < .8 * c_lambda or q_time < .8 * q_lambda:
             cont = False
         return c_time, q_time, cont
 
@@ -176,20 +188,25 @@ def main():
                             "leda_values_v.json")
     leda_values_v_by_level = load_from_json(filename)
 
-    leda_vals_new, isd_values_to_compute = post_process_values(
+    leda_vals_new, isd_values_ok, isd_values_to_compute = post_process_values(
         leda_values_t_by_level, leda_values_v_by_level)
 
-    
-    print(f"Saving isd vals fo compute {isd_values_to_compute}")
+    print("Saving isd vals ok")
     filename = os.path.join("out", "values", "from_restrictions_3",
                             "isd_values.json")
+    isd_vals = sorted(set(isd_values_ok))
+    save_to_json(filename, isd_vals, cls=ISDValueEncoder)
+
+    print(f"Saving isd vals fo compute")
+    filename = os.path.join("out", "values", "from_restrictions_3",
+                            "isd_values_to_compute.json")
     isd_vals = sorted(set(isd_values_to_compute))
     save_to_json(filename, isd_vals, cls=ISDValueEncoder)
 
     filename = os.path.join("out", "values", "from_restrictions_3",
                             "leda_values_")
     for level, item in zip((1, 3, 5), leda_vals_new.items()):
-        level_idx, leda_vals = item
+        _, leda_vals = item
         filename_level = filename + f"L{level}.json"
         print(f"Saving leda vals for {level}")
         save_to_json(filename_level, leda_vals, cls=LEDAValueEncoder)
