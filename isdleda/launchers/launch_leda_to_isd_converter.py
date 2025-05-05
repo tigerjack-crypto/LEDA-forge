44f546599975499762d445a55e6af71012c9375a
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-05-05 18:52:30 +0200

    MOD leda to isd converter

--- isdleda/launchers/launch_leda_to_isd_converter.py
+++ isdleda/launchers/launch_leda_to_isd_converter.py
@@ -1,16 +1,22 @@
-from isdleda.launchers.launcher_utils import OUT_DIR
-from isdleda.utils.export.export import ISDValueEncoder, from_csv_to_ledavalue, save_to_json
-from isdleda.utils.common import ISDValue
-from typing import List
-import os
+"""Given a list (in csv) of leda values, output the ISD attack list"""
 
-# the 0-th time this is launched
-ITERATION=0
+import os
+from sys import argv
+from typing import List
+
+from isdleda.launchers.launcher_utils import OUT_DIR
+from isdleda.utils.common import ISDValue
+from isdleda.utils.export.export import (ISDValueEncoder,
+                                         from_csv_to_ledavalue, save_to_json)
+
 
 def main():
+    stage = argv[1]  # the stage in which we are in
+    input_dir = argv[2]  # The directory containing the CSV files
     isd_values: List[ISDValue] = []
     for level in (1, 3, 5):
-        filename = f"{OUT_DIR}/post_dfr_in/{ITERATION}/cat_{level}_region"
+        filename = f"{input_dir}/cat_{level}_region"
+        # filename = f"{OUT_DIR}/post_dfr_in/{ITERATION}/cat_{level}_region"
         print(f"Analayzing {filename}")
         ledavalues = from_csv_to_ledavalue(f"{filename}.csv")
 
@@ -42,10 +48,11 @@ def main():
             isd_values.append(ISDValue(n, n - k, t, msgs=[f"KRA 3"]))
 
     print(len(isd_values))
-    filename = os.path.join(OUT_DIR, "isd-leda", "values",
-                            f"post_dfr_out", f"{ITERATION}", "isd_values.json")
+    filename = os.path.join(OUT_DIR, "isd-leda", "values", f"S{stage}"
+                            "exhaustive_generation", "isd_values.json")
     print(f"Output file {filename}")
     save_to_json(filename, isd_values, cls=ISDValueEncoder)
+
 
 if __name__ == '__main__':
     main()
