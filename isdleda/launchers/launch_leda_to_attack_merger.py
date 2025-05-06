f1fe9b772ccddbadd77901703427a23959dff9b3
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-05-06 13:48:35 +0200

    ADD threshold to merger

--- isdleda/launchers/launch_leda_to_attack_merger.py
+++ isdleda/launchers/launch_leda_to_attack_merger.py
@@ -25,20 +25,18 @@ def write_to_csv(filename, values):
         writer.writerows(values)
 
 
-def check_dataset(attack_dir, isd_val, c_lambda, q_lambda, reduction, msg):
+def check_dataset(attack_dir, isd_val, reduction, msg):
     filename = os.path.join(
         attack_dir,
         f"{isd_val.n:06}_{isd_val.n - isd_val.r:06}_{isd_val.t:03}.json")
     # print(filename)
     # continue exploring to find another minimum
     # filename = f"out/ledatools/json/{n:06}_{k:06}_{t:03}.json"
-    less_than_threshold = False
     try:
         data = load_from_json(filename)
     except:
         print(filename)
-        return 0, 0, 0
-
+        return 0, 0
 
     c_time = data['Classic']['Plain']['value'] - reduction
     q_time = (data['Quantum']['Plain']['value']) * 2 - reduction
@@ -47,18 +45,19 @@ def check_dataset(attack_dir, isd_val, c_lambda, q_lambda, reduction, msg):
         raise Exception(
             f"Value less than 0 for {filename}, with reduction {reduction}: {c_time}, {q_time}"
         )
-    if c_time < .8 * c_lambda or q_time < .75 * q_lambda:
-        less_than_threshold = True
-    return c_time, q_time, less_than_threshold
+    return c_time, q_time
 
 
 def main():
     # the stage in which we are in
-    stage = argv[1]
+    stage = int(argv[1])
     # The directory containing the CSV files of LEDA values
     input_dir = argv[2]
     # The directory containing the json files containing ISD attacksk_dir = argv[
     attack_dir = argv[3]
+    # if to check for the threshold, boolean
+    check_threshold = bool(argv[4])
+
     output_dir = os.path.join(f"{OUT_DIR}", "isd-leda", "values", f"S{stage}")
     counter = get_pass_counter(output_dir)
     _tmp = os.path.join(output_dir, f"{counter}_leda2attack")
@@ -91,11 +90,9 @@ def main():
             # MRA
             isd_val = get_mra_from_leda(leda_val)
             try:
-                c_aes, q_aes, _ = check_dataset(
+                c_aes, q_aes = check_dataset(
                     attack_dir,
                     isd_val,
-                    c_lambda=c_lambda,
-                    q_lambda=q_lambda,
                     reduction=get_qc_reduction_mra(leda_val),
                     msg=f"MRA, {leda_val.p} {leda_val.n0} {leda_val.v}")
             except FileNotFoundError:
@@ -111,11 +108,9 @@ def main():
             # KRA 1
             isd_val = get_kra1_from_leda(leda_val)
             try:
-                c_aes, q_aes, _ = check_dataset(
+                c_aes, q_aes = check_dataset(
                     attack_dir,
                     isd_val,
-                    c_lambda=c_lambda,
-                    q_lambda=q_lambda,
                     reduction=get_qc_reduction_kra1(leda_val),
                     msg=f"KRA1, {leda_val.p} {leda_val.n0} {leda_val.v}")
             except FileNotFoundError:
@@ -132,11 +127,9 @@ def main():
             if leda_val.n0 != 2:
                 isd_val = get_kra2_from_leda(leda_val)
                 try:
-                    c_aes, q_aes, _ = check_dataset(
+                    c_aes, q_aes = check_dataset(
                         attack_dir,
                         isd_val,
-                        c_lambda=c_lambda,
-                        q_lambda=q_lambda,
                         reduction=get_qc_reduction_kra2(leda_val),
                         msg=f"KRA2, {leda_val.p} {leda_val.n0} {leda_val.v}")
                 except FileNotFoundError:
@@ -154,11 +147,9 @@ def main():
             # KRA 3
             isd_val = get_kra3_from_leda(leda_val)
             try:
-                c_aes, q_aes, _ = check_dataset(
+                c_aes, q_aes = check_dataset(
                     attack_dir,
                     isd_val,
-                    c_lambda=c_lambda,
-                    q_lambda=q_lambda,
                     reduction=get_qc_reduction_kra3(leda_val),
                     msg=f"KRA3, {leda_val.p} {leda_val.n0} {leda_val.v}")
             except FileNotFoundError:
@@ -173,9 +164,10 @@ def main():
             # min c, min q, min c attack, min q attack
             min_c = min(c_values)
             min_q = min(q_values)
-            csv_value.append(min_c)
-            csv_value.append(min_q)
-            csv_values.append(csv_value)
+            if check_threshold and (min_c > .75 * c_lambda or min_q > .75 * q_lambda):
+                csv_value.append(min_c)
+                csv_value.append(min_q)
+                csv_values.append(csv_value)
 
         write_to_csv(filename_out, csv_values)
     set_pass_counter(output_dir, counter + 1)
