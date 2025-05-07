273e674263b49fd271c3b81e8dee2ec58266499b
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-05-07 15:44:14 +0200

    MOD exhaustive values generation, parallel

--- isdleda/launchers/launch_values_generation.py
+++ isdleda/launchers/launch_values_generation.py
@@ -6,15 +6,16 @@ import os
 from collections import defaultdict
 from dataclasses import asdict
 from itertools import product
+# from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Pool
 from sys import argv
 from typing import Dict, List, Set
 
 import numpy as np
-from isdleda.launchers.launcher_utils import (LEVELS, OUT_DIR, get_kra1_from_leda, get_kra2_from_leda, get_kra3_from_leda, get_mra_from_leda,
+from isdleda.launchers.launcher_utils import (LEVELS, OUT_DIR,
                                               get_proper_leda_primes)
-from isdleda.utils.common import ISDValue, LEDAValue
-from isdleda.utils.export.export import (ISDValueEncoder,
-                                         save_ledavalues_to_csv, save_to_json)
+from isdleda.utils.common import LEDAValue
+from isdleda.utils.export.export import save_ledavalues_to_csv
 
 
 class CustomEncoder(json.JSONEncoder):
@@ -44,6 +45,8 @@ def merge_leda_values(
 
             # Cartesian product of all t's and v's
             for t_val, v_val in product(t_vals, v_vals):
+                if v_val.p % 2 == 0:
+                    print(f"v is even in merging {t_val} {v_val}")
                 merged_val = LEDAValue(
                     p=t_val.p if t_val.p is not None else v_val.p,
                     n0=t_val.n0 if t_val.n0 is not None else v_val.n0,
@@ -56,102 +59,115 @@ def merge_leda_values(
     return merged
 
 
+def worker(leda_primes, c_lambda):
+    # level -> p_n0; all values have t != -1, v == -1
+    leda_values_v: Dict[str, Set[LEDAValue]] = defaultdict(set)
+    leda_values_t: Dict[str, Set[LEDAValue]] = defaultdict(set)
+
+    for prime in filter(lambda p: 5e3 < p < 9e4, leda_primes):
+        for n0 in range(2, 6):
+            # MRA, KRA1, 2, 3
+
+            n = prime * n0
+            if n > 2e5:
+                continue
+            # MRA, KRA1, KRA2, KRA3
+            r = prime
+            k = n - r
+            c = -np.log2(1 - k / n)
+            c_lambda_expected = c_lambda - 3 * np.log2(r)
+            tmin = int(np.floor(0.6 * c_lambda_expected / c))
+            tmax = int(np.ceil(1.7 * c_lambda_expected / c))
+
+            for t in range(tmin, tmax, 1):
+                leda_val = LEDAValue(prime, n0, t, -1, msgs=[f"MRA"])
+                leda_values_t[f"{prime}_{n0}"].add(leda_val)
+                # isd_values.append(get_mra_from_leda(leda_val))
+            del n, k, r, c, c_lambda_expected, t, leda_val
+
+            # KRA1
+            n = prime * n0
+            r = prime
+            k = n - r
+            c = -np.log2(1 - k / n)
+            c_lambda_expected = c_lambda - 3 * np.log2(r)
+
+            vmin = int(np.floor(0.6 * c_lambda_expected / (2 * c)))
+            vmax = int(np.ceil(1.7 * c_lambda_expected / (2 * c)))
+            if vmin % 2 == 0:
+                vmin -= 1
+            if vmax % 2 == 0:
+                vmax += 1
+            for v in range(vmin, vmax, 2):
+                if v % 2 == 0:
+                    print(f"KRA1, v is {v}")
+                leda_val = LEDAValue(prime, n0, -1, v, msgs=[f"KRA 1"])
+                leda_values_v[f"{prime}_{n0}"].add(leda_val)
+                # isd_values.append(get_kra1_from_leda(leda_val))
+            del n, k, r, c, c_lambda_expected, v, leda_val
+
+            # KRA2
+            if n0 != 2:
+                n = 2 * prime
+                r = prime
+                k = n - r
+                c = -np.log2(1 - k / n)
+                c_lambda_expected = c_lambda - 3 * np.log2(r)
+
+                vmin = int(np.floor(0.6 * c_lambda_expected / (2 * c)))
+                vmax = int(np.ceil(1.7 * c_lambda_expected / (2 * c)))
+                if vmin % 2 == 0:
+                    vmin -= 1
+                if vmax % 2 == 0:
+                    vmax += 1
+                for v in range(vmin, vmax, 2):
+                    if v % 2 == 0:
+                        print(f"KRA2, v is {v}")
+                    leda_val = LEDAValue(prime, n0, -1, v, msgs=[f"KRA 2"])
+                    leda_values_v[f"{prime}_{n0}"].add(leda_val)
+                    # isd_values.append(
+                    #     get_kra2_from_leda(leda_val))
+                del n, k, r, c, c_lambda_expected, v, leda_val
+
+            # KRA3
+            n = prime * n0
+            r = prime * (n0 - 1)
+            k = n - r
+            c = -np.log2(1 - k / n)
+            c_lambda_expected = c_lambda - 3 * np.log2(r)
+            vmin = int(np.floor(0.6 * c_lambda_expected / (n0 * c)))
+            vmax = int(np.ceil(1.7 * c_lambda_expected / (n0 * c)))
+            if vmin % 2 == 0:
+                vmin -= 1
+            if vmax % 2 == 0:
+                vmax += 1
+            for v in range(vmin, vmax, 2):
+                if v % 2 == 0:
+                    print(f"KRA3, v is {v}")
+                leda_val = LEDAValue(prime, n0, -1, v, msgs=[f"KRA 3"])
+                leda_values_v[f"{prime}_{n0}"].add(leda_val)
+                # isd_values.append(get_kra3_from_leda(leda_val))
+    return leda_values_t, leda_values_v
+
+
 def main():
     stage = argv[1]  # the stage in which we are in
-
     leda_primes = get_proper_leda_primes()
-
-    isd_values: List[ISDValue] = []
-    # level -> p_n0; all values have t != -1, v == -1
     leda_values_t_by_level: Dict[int, Dict[str, Set[LEDAValue]]] = defaultdict(
         lambda: defaultdict(set))
     # level -> p_n0; all values have v != -1, t == -1
     leda_values_v_by_level: Dict[int, Dict[str, Set[LEDAValue]]] = defaultdict(
         lambda: defaultdict(set))
 
-    for level_idx, c_lambda in enumerate(LEVELS):
-        # for c_lambda in LEVELS:
-        for prime in filter(lambda p: 5e3 < p < 9e4, leda_primes):
-            for n0 in range(2, 6):
-                # MRA, KRA1, 2, 3
+    args = [(leda_primes, c_lambda)
+            for c_lambda in LEVELS]
 
-                n = prime * n0
-                if n > 2e5:
-                    continue
-                # MRA, KRA1, KRA2, KRA3
-                r = prime
-                k = n - r
-                c = -np.log2(1 - k / n)
-                c_lambda_expected = c_lambda - 3 * np.log2(r)
-                tmin = int(np.floor(.6 * c_lambda_expected / c))
-                tmax = int(np.ceil(1.8 * c_lambda_expected / c))
+    with Pool() as pool:
+        results = pool.starmap(worker, args)
 
-                for t in range(tmin, tmax, 1):
-                    leda_val = LEDAValue(prime, n0, t, -1, msgs=[f"MRA"])
-                    leda_values_t_by_level[level_idx][f"{prime}_{n0}"].add(
-                        leda_val)
-                    isd_values.append(get_mra_from_leda(leda_val))
-                del n, k, r, c, c_lambda_expected, t, leda_val
-
-                # KRA1
-                n = prime * n0
-                r = prime
-                k = n - r
-                c = -np.log2(1 - k / n)
-                c_lambda_expected = c_lambda - 3 * np.log2(r)
-
-                vmin = int(np.floor(.6 * c_lambda_expected / (2 * c)))
-                vmax = int(np.ceil(1.8 * c_lambda_expected / (2 * c)))
-                if vmin % 2 == 0:
-                    vmin -= 1
-                if vmax % 2 == 0:
-                    vmax += 1
-                for v in range(vmin, vmax, 2):
-                    leda_val = LEDAValue(prime, n0, -1, v, msgs=[f"KRA 1"])
-                    leda_values_v_by_level[level_idx][f"{prime}_{n0}"].add(
-                        leda_val)
-                    isd_values.append(get_kra1_from_leda(leda_val))
-                del n, k, r, c, c_lambda_expected, v, leda_val
-
-                # KRA2
-                if n0 != 2:
-                    n = 2 * prime
-                    r = prime
-                    k = n - r
-                    c = -np.log2(1 - k / n)
-                    c_lambda_expected = c_lambda - 3 * np.log2(r)
-
-                    vmin = int(np.floor(.6 * c_lambda_expected / (2 * c)))
-                    vmax = int(np.ceil(1.8 * c_lambda_expected / (2 * c)))
-                    if vmin % 2 == 0:
-                        vmin -= 1
-                    if vmax % 2 == 0:
-                        vmax += 1
-                    for v in range(vmin, vmax, 2):
-                        leda_val = LEDAValue(prime, n0, -1, v, msgs=[f"KRA 2"])
-                        leda_values_v_by_level[level_idx][f"{prime}_{n0}"].add(
-                            leda_val)
-                        isd_values.append(
-                            get_kra2_from_leda(leda_val))
-                    del n, k, r, c, c_lambda_expected, v, leda_val
-
-                # KRA3
-                n = prime * n0
-                r = prime * (n0 - 1)
-                k = n - r
-                c = -np.log2(1 - k / n)
-                c_lambda_expected = c_lambda - 3 * np.log2(r)
-                vmin = int(np.floor(.6 * c_lambda_expected / (n0 * c)))
-                vmax = int(np.ceil(1.8 * c_lambda_expected / (n0 * c)))
-                if vmin % 2 == 0:
-                    vmin -= 1
-                if vmax % 2 == 0:
-                    vmax += 1
-                for v in range(vmin, vmax, 2):
-                    leda_val = LEDAValue(prime, n0, -1, v, msgs=[f"KRA 3"])
-                    leda_values_v_by_level[level_idx][f"{prime}_{n0}"].add(
-                        leda_val)
-                    isd_values.append(get_kra3_from_leda(leda_val))
+    for idx, (leda_t, leda_v) in enumerate(results):
+        leda_values_t_by_level[idx] = leda_t
+        leda_values_v_by_level[idx] = leda_v
 
     print("Exhaustive generation over")
     dirpath = os.path.join(
@@ -159,7 +175,7 @@ def main():
         "isd-leda",
         "values",
         f"S{stage}",
-        "exhaustive_generation",
+        "0_exhaustive_generation",
     )
     levels = [1, 3, 5]
     merged_leda_values = merge_leda_values(leda_values_t_by_level,
@@ -170,10 +186,10 @@ def main():
         print(f"Saving leda vals to {filename}")
         save_ledavalues_to_csv(merged_leda_values[level_idx], filename)
 
-    filename = os.path.join(dirpath, "isd_values")
-    isd_vals = sorted(set(isd_values))
-    print(f"Saving isd vals to {filename}")
-    save_to_json(filename, isd_vals, cls=ISDValueEncoder)
+    # filename = os.path.join(dirpath, "isd_values")
+    # isd_vals = sorted(set(isd_values))
+    # print(f"Saving isd vals to {filename}")
+    # save_to_json(filename, isd_vals, cls=ISDValueEncoder)
 
 
 if __name__ == '__main__':
