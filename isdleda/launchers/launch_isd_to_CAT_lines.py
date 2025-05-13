a47e164fc117fb0437fe1278b382d24fe3e58ac0
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-05-13 19:56:49 +0200

    ADD tool to convert from CAT lines to best attack value

---
+++ isdleda/launchers/launch_isd_to_CAT_lines.py
@@ -0,0 +1,13 @@
+from isdleda.utils.export.export import load_from_json
+from sys import argv
+
+# Parse the JSON data
+
+data = load_from_json(argv[1])
+
+# Output the required lines
+for entry in data:
+    n_value = entry['n']
+    k_value = entry['k']
+    w_value = entry['w']
+    print(f"N={n_value},K={k_value},W={w_value}")
