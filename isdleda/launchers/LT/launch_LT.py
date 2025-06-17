b79b97963b411de4677fa1f5f93f95fc0fef4428
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-06-17 18:42:15 +0200

    ADD LT launcher

--- isdleda/launchers/LT/launch_LT.py
+++ isdleda/launchers/LT/launch_LT.py
@@ -1,13 +1,15 @@
 """Simple script to launch the LEDATools work_factor_computation script.
 As an example, the script can be launched as
 
---threads 32 --json ../leda_design/stime_ISD/isd-leda/values/S3/2_leda2isd/isd_values.json --out BJMM
+--threads 32 --json ../leda_design/stime_ISD/orchestra/values/S3/2_leda2isd/isd_values.json --out BJMM
 
 """
 import argparse
 import subprocess
 import os
-from pathlib import Path
+
+from isdleda.utils.paths import OUT_DIR
+# from pathlib import Path
 
 
 def main():
@@ -25,26 +27,40 @@ def main():
                         type=str,
                         required=True,
                         help='Output file name')
-    parser.add_argument('--bin-dir',
-                        type=str,
-                        required=True,
-                        help='Path to the directory containing the bins')
+    # parser.add_argument('--bin-dir',
+    #                     type=str,
+    #                     required=True,
+    #                     help='Path to the directory containing the bins')
     args = parser.parse_args()
 
     env = os.environ.copy()
     env['OMP_NUM_THREADS'] = str(args.threads)
 
-    # Define the path to the executable relative to this script
-    exec_path = Path(
-        __file__).parent.resolve() / '../A/B/C/bin/work_factor_computation'
-    exec_path = exec_path.resolve()  # resolve to absolute path
+    # # Define the path to the executable relative to this script
+    # exec_path = Path(
+    #     __file__).parent.resolve() / '../A/B/C/bin/work_factor_computation'
+    # exec_path = exec_path.resolve()  # resolve to absolute path
 
-    if not exec_path.exists():
-        raise FileNotFoundError(f"Executable not found at {exec_path}")
+    # if not exec_path.exists():
+    #     raise FileNotFoundError(f"Executable not found at {exec_path}")
 
-    command = [str(exec_path), '--json', args.json, '--out', args.out]
+    command = ['work_factor_computation', '--json', args.json, '--out-dir', os.path.join(OUT_DIR, 'LT') , '--out', args.out]
+    print(' '.join(command))
 
-    subprocess.run(command, env=env, check=True)
+    try:
+        res = subprocess.run(command, env=env, check=True, capture_output=True, text=True)
+        # Optional: Check stderr even if returncode == 0
+        if res.stderr:
+            print("Command succeeded, but there were warnings/errors in stderr:")
+            print(res.stderr)
+    except subprocess.CalledProcessError as e:
+        print("Command failed:")
+        print("Return code:", e.returncode)
+        print("Standard error:")
+        print(e.stderr)
+        print("Standard output:")
+        print(e.stdout)
+
 
 
 if __name__ == '__main__':
