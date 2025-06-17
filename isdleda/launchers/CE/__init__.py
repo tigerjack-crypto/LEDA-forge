c82d62b10cb76f1e431a1337b83698b48c1326ac
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-06-17 17:00:34 +0200

    FIX CE imports
    - Mock prettytable
    - Add run example
    - Add submodule to path for CE

---
+++ isdleda/launchers/CE/__init__.py
@@ -0,0 +1,22 @@
+# to include submodules in the PATH
+import os
+import sys
+from unittest.mock import MagicMock
+
+# Create a MagicMock for the prettytable module
+print("WARN: Mocking unused pretty-table")
+mock_prettytable = MagicMock()
+mock_table_instance = MagicMock()
+# Mock the PrettyTable class to return a mock instance
+mock_prettytable.PrettyTable.return_value = mock_table_instance
+# Inject the mocked module into sys.modules
+sys.modules['prettytable'] = mock_prettytable
+
+# Add CE submodule to path
+thisdir = os.path.dirname(os.path.realpath(__file__))
+# print(thisdir)
+sys.path.append(
+    os.path.join(thisdir, "..", "..", "submodules",
+                 "cryptographic_estimators"))
+# sys.path.append(os.path.join(thisdir, "..", "..", "submodules", "isd_scripts"))
+print(f"INFO: sys path is {sys.path}")
