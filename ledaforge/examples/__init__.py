32c1a0a2f23c274d30d1e4f43f1d4f590ed08ccc
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2025-06-20 10:58:41 +0200

    Minors

---
+++ ledaforge/examples/__init__.py
@@ -0,0 +1,9 @@
+# to include CryptographicEstimators in the PATH
+import os
+import sys
+
+thisdir = os.path.dirname(os.path.realpath(__file__))
+print(thisdir)
+sys.path.append(os.path.join(thisdir, "..", "..", "submodules", "cryptographic_estimators"))
+print(sys.path)
+
